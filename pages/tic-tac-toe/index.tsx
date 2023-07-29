578a5a00123ecb880bfccb449c61644a6f568e99
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-07-29 13:13:23 -0300

    feat: improving the game

--- pages/tic-tac-toe/index.tsx
+++ pages/tic-tac-toe/index.tsx
@@ -1,42 +1,70 @@
 import React, { useState } from "react";
 
+const WINNER_BOARDS = [
+  "111000000",
+  "000111000",
+  "000000111",
+  "100100100",
+  "010010010",
+  "001001001",
+  "100010001",
+  "001010100",
+];
+
+const INITIAL_BOARD = [
+  ["[ ]", "[ ]", "[ ]"],
+  ["[ ]", "[ ]", "[ ]"],
+  ["[ ]", "[ ]", "[ ]"],
+];
+
+const INITIAL_STATE = {
+  board: INITIAL_BOARD,
+  currentPlayer: "x",
+};
+
 const index = () => {
-  const [state, setState] = useState({
-    board: [
-      ["[ ]", "[ ]", "[ ]"],
-      ["[ ]", "[ ]", "[ ]"],
-      ["[ ]", "[ ]", "[ ]"],
-    ],
-    currentPlayer: "x",
-  });
+  const [state, setState] = useState(INITIAL_STATE);
 
   const togglePlayer = () => {
     const currentPlayer = state.currentPlayer === "x" ? "o" : "x";
     setState((prev) => ({ ...prev, currentPlayer }));
   };
 
-  const mutateBoard = (row: number, col: number, player: string) => {
-    let mutableBoard = state.board;
+  const updateBoard = (row: number, col: number, player: string) => {
+    let mutableBoard = [...state.board];
     mutableBoard[row][col] = player;
     setState((prev) => ({ ...prev, board: mutableBoard }));
   };
 
+  const verifyWinner = (board: string[][], player: string) => {
+    const parsedBoard = board.map((row) => {
+      return row.map((col) => (col === player ? "1" : "0"));
+    });
+    const flattened = parsedBoard.flat(1).join("");
+    const isWinner = WINNER_BOARDS.includes(flattened);
+    return isWinner;
+  };
+
+  const hasAWinner = (board: string[][]): string | undefined => {
+    const isXWinner = verifyWinner(board, "x");
+    const isOWinner = verifyWinner(board, "o");
+    const winner = isXWinner ? "x" : isOWinner ? "o" : undefined;
+    return winner;
+  };
+
   const handlePlay = (row: number, col: number) => {
-    mutateBoard(row, col, state.currentPlayer);
+    if (!!winner) return;
+    updateBoard(row, col, state.currentPlayer);
     togglePlayer();
   };
 
-  const verifyLine = (line: string[]) => {
-    return line.every((entry) => entry === "o" || entry === "x");
+  const handleRestartGame = () => {
+    setState(() => INITIAL_STATE);
+    console.log(state);
+    console.log("restarted");
   };
 
-  const verifyWinner = (board: string[][]): "o" | "x" => {
-    return "o";
-  };
-
-  const winner = verifyWinner(state.board);
-
-  console.log(winner);
+  const winner = hasAWinner(state.board);
 
   return (
     <div>
@@ -52,6 +80,8 @@ const index = () => {
           ))}
         </div>
       ))}
+      <p>{winner}</p>
+      <button onClick={handleRestartGame}>Restart Game</button>
     </div>
   );
 };
